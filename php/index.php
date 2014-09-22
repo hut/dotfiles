e11c06b76a4654fde993977a4a8384e739ed456e
hut <hut@lepus.uberspace.de>
2014-09-22 15:31:14 +0200

    tweaking index.php

--- php/index.php
+++ php/index.php
@@ -4,22 +4,26 @@ $dir = opendir ('.');
 $ignore = array ('index.php');
 define (br, "\n");
 define (sl, "/");
+define (wid, 50);
 $color = array('eeeeff', 'ffffff');
 
 $dirs = array('..');
 $files = array();
+$total_size = 0;
 
 while ($item = readdir($dir))
     if (!in_array ($item, $ignore) && $item[0] != '.')
         if (is_dir($item))
             $dirs[] = $item;
-        else
+        else {
             $files[] = $item;
+            $total_size += filesize($item);
+        }
 
 sort($dirs);
 sort($files);
 
-print '<pre>index of '.dirname ($_SERVER['PHP_SELF']).br.br;
+print '<pre>index of '.dirname ($_SERVER['PHP_SELF']).' ['.floor($total_size/1024).'K]'.br.br;
 
 $i = 0;
 foreach (array_merge($dirs, $files) as $key => $val) {
@@ -30,7 +34,7 @@ foreach (array_merge($dirs, $files) as $key => $val) {
         $size = filesize ($val);
         if ($size > 1024)
             $size = floor($size / 1024)."K";
-        print "<a href='$val'>".str_pad ($val."</a>", (90-strlen($size))).$size.br;
+        print "<a href='$val'>".str_pad ($val."</a>", (wid-strlen($size))).$size.br;
     }
     $i++;
     print '</div>';
