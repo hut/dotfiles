c45acb631c6e4f6d6288b356b76533f6f3554f40
hut <hut@lepus.uberspace.de>
2015-11-26 23:53:53 +0100

    added rangerrc

--- deploy/payload/install_configs.sh
+++ deploy/payload/install_configs.sh
@@ -5,5 +5,9 @@ cp --backup=numbered -v vimrc ~/.vimrc
 cp --backup=numbered -v screenrc ~/.screenrc
 cp --backup=numbered -v tmux.conf ~/.tmux.conf
 cp --backup=numbered -v bashrc ~/.bashrc_hut
+
+mkdir -p ~/.config/ranger
+cp --backup=numbered -v rangerrc ~/.config/ranger/rc.conf
+
 t='source ~/.bashrc_hut'
 grep -q -F "$t" ~/.bashrc || echo "$t" >> ~/.bashrc
