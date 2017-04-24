8777bf89c7c6ec31d31ba71acf39425dbb9c3c5c
hut <hut@lepus.uberspace.de>
2017-04-24 02:07:09 +0200

    added pythonlib/prefs.py

---
+++ pythonlib/prefs.py
@@ -0,0 +1,8 @@
+dmenu = ['dmenu', '-f', '-i',
+        '-l', '40',
+        #'-fn', 'Yelena1:pixelsize=12',
+        '-fn', 'Bitstream Vera Sans:pixelsize=14',
+        '-nb', '#000000',
+        '-nf', '#aa5533',
+        '-sb', '#885533',
+        '-sf', '#ffffff']
