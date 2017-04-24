6861f8159497b1426dadb726721735ac295649fd
hut <hut@lepus.uberspace.de>
2017-04-24 02:09:11 +0200

    added pythonlib/stringlib.py

---
+++ pythonlib/stringlib.py
@@ -0,0 +1,25 @@
+import re as _re
+
+
+def grep(what, regexp):
+    if isinstance(what, str):
+        match = _re.search(regexp, what)
+        if match:
+            return match.group(0)
+        else:
+            return what
+    else:
+        cls = what.__class__
+        result = []
+        for item in what:
+            if _re.search(regexp, item):
+                result.append(item)
+        return cls(result)
+
+
+def sed(string, pattern, replace, count=0):
+    if isinstance(string, str):
+        return _re.sub(pattern, replace, string, count=count)
+    elif hasattr(string, '__iter__'):
+        return list(sed(item, pattern, replace, count=count)
+                for item in string)
