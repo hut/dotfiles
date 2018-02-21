f195211b6f7bd7b0e082f3341e2160aa2c42c9f0
hut <hut@hut.pm>
2018-02-21 09:33:28 +0100

    set viper fast keyseq timeout

--- config/emacs/init.el
+++ config/emacs/init.el
@@ -6,3 +6,16 @@
 (org-babel-load-file "~/emacs/configuration.org")
 (global-set-key "\C-ca" 'org-agenda)
 
+(custom-set-variables
+ ;; custom-set-variables was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ '(delete-selection-mode nil)
+ '(viper-fast-keyseq-timeout 10))
+(custom-set-faces
+ ;; custom-set-faces was added by Custom.
+ ;; If you edit it by hand, you could mess it up, so be careful.
+ ;; Your init file should contain only one such instance.
+ ;; If there is more than one, they won't work right.
+ )
